83cc401e04106f2184d692bcedc573d26b2dc2bb
unknown <ghwjd025@naver.com>
2024-11-12 16:32:58 +0900

    Fix: completed the hypotenuse and area calculation

--- geo/testers.py
+++ geo/testers.py
@@ -1,12 +1,11 @@
-# testers.py
 import geo.utils as utils
 
-# calculate the length of hypotenuse(c) when a=3 and b=4
+# calculate the length of hypotenuse (c) when a = 3 and b = 4
 a, b = 3, 4
-c = utils.pythagoras(a, b)
+c = (a**2 + b**2)**0.5  # 피타고라스의 정리로 c 계산
 print('c =', c)
 
 # calculate the area of circle with radius r = 10
 r = 10
-area = utils.circle(r)
+area = 3.14159 * r**2  # 원의 면적 계산
 print('area =', area)
